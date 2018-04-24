b9564f7c5912d06d5fe5c804f610b790fe62e4ce
hibikiitoga <itogahibiki@gmail.com>
2018-04-24 18:05:22 +0900

    func. for random sampling points

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <boost/lexical_cast.hpp>
 #include "Vector3D.hpp"
 #include "constexpr_math.hpp"
+std::mt19937_64 mt(456981365819651);
 
 constexpr double Radius_of_Sphere = 10.0;
 
@@ -19,6 +20,21 @@ void projection(std::vector<Vector3D>& ps)
       ps.at(i)*=Radius_of_Sphere;
    }
    return ;
+}
+
+Vector3D random_base_point()
+{
+   std::uniform_real_distribution<double> dist(-1.0, +1.0);
+   Vector3D res;
+   do
+   {
+      res.x=dist(mt); 
+      res.y=dist(mt); 
+      res.z=dist(mt); 
+   }while(res.norm()>1.0);
+   res/=res.norm();
+   res*=Radius_of_Sphere;
+   return res;
 }
 
 int main(int argc, char* argv[])

cc1a983862c768ca336c3add919b1dbcb6c51802
hibikiitoga <itogahibiki@gmail.com>
2018-04-24 17:58:04 +0900

    Projection to sphere

--- main.cpp
+++ main.cpp
@@ -9,7 +9,22 @@
 #include "Vector3D.hpp"
 #include "constexpr_math.hpp"
 
-int main()
+constexpr double Radius_of_Sphere = 10.0;
+
+void projection(std::vector<Vector3D>& ps)
 {
-   
+   for(size_t i=0,i_size=ps.size();i<i_size;++i)
+   {
+      ps.at(i)/=ps.at(i).norm();
+      ps.at(i)*=Radius_of_Sphere;
+   }
+   return ;
+}
+
+int main(int argc, char* argv[])
+{
+   std::vector<std::string> argments;
+   for(int i=0;i<argc;++i){ argments.push_back(argv[i]); }
+  
+   return EXIT_SUCCESS;
 }
